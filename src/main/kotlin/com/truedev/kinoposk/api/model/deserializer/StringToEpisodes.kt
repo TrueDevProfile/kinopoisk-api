7e9b32a9d76b4eb171913615ab48ef60c1122c7b
TrueDevProfile <andreyusa08@gmail.com>
2020-09-08 14:50:12 +0400

    feature: add tv shows

--- src/main/kotlin/com/truedev/kinoposk/api/model/deserializer/StringToEpisodes.kt
+++ src/main/kotlin/com/truedev/kinoposk/api/model/deserializer/StringToEpisodes.kt
@@ -15,22 +15,17 @@ class StringToEpisodes : JsonDeserializer<List<Episode>>() {
         return (node as ArrayNode).toList()
             .map { it.get("episode") }
             .map {
-                try {
-                    Episode(
-                        id = it.get("id").asInt(),
-                        seasonNumber = it.get("seasonNumber").asInt(),
-                        episodeNumber = it.get("episodeNumber").asInt(),
-                        name = it.get("name").asText().ifBlank { null },
-                        nameEng = it.get("nameEng").asText().ifBlank { null },
-                        synopsis = it.get("synopsis")?.asText(),
-                        releaseDate = it.get("releaseDate")
-                            ?.let { releaseDate -> LocalDate.parse(releaseDate.asText()) },
-                        dateAccuracy = it.get("dateAccuracy")?.asText()
-                    )
-                } catch (e: java.lang.NullPointerException) {
-                    println(it)
-                    throw RuntimeException()
-                }
+                Episode(
+                    id = it.get("id").asInt(),
+                    seasonNumber = it.get("seasonNumber").asInt(),
+                    episodeNumber = it.get("episodeNumber").asInt(),
+                    name = it.get("name").asText().ifBlank { null },
+                    nameEng = it.get("nameEng").asText().ifBlank { null },
+                    synopsis = it.get("synopsis")?.asText(),
+                    releaseDate = it.get("releaseDate")
+                        ?.let { releaseDate -> LocalDate.parse(releaseDate.asText()) },
+                    dateAccuracy = it.get("dateAccuracy")?.asText()
+                )
             }
     }
 }
