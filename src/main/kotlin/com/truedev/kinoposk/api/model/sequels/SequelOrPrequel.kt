416792c79f7523294dd785cb5b471cc3baa7eab7
Andrey <andreyusa08@gmail.com>
2020-10-03 16:59:41 +0400

    Feature/kpa 373 sequels (#69)
    * feature: KPA-373 sequels and prequels
    
    * feature: KPA-373 fix deserialization
    
    * feature: KPA-373 resolve conflicts

--- src/main/kotlin/com/truedev/kinoposk/api/model/sequels/SequelOrPrequel.kt
+++ src/main/kotlin/com/truedev/kinoposk/api/model/sequels/SequelOrPrequel.kt
@@ -1,12 +1,13 @@
 package com.truedev.kinoposk.api.model.sequels
 
 enum class SequelOrPrequel {
-    SEQUEL, PREQUEL, UNKNOWN;
+    SEQUEL, PREQUEL, REMAKE, UNKNOWN;
 
     companion object {
         fun fromString(value: String): SequelOrPrequel = when (value) {
             "Начало" -> SequelOrPrequel.PREQUEL
             "Продолжение" -> SequelOrPrequel.SEQUEL
+            "Ремейк" -> SequelOrPrequel.REMAKE
             else -> SequelOrPrequel.UNKNOWN
         }
     }
